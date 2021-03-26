2e85dd94bedc50e84232b5a72129c84a809d9776
hayrapetyan8768 <72487857+hayrapetyan8768@users.noreply.github.com>
2021-03-26 22:42:06 +0400

    Add files via upload

---
+++ Crawler/WebsiteRepository.hpp
@@ -0,0 +1,30 @@
+#ifndef WEBSITEREPOSITORY_HPP
+#define WEBSITEREPOSITORY_HPP
+
+#include <vector>
+#include "Website.hpp"
+
+class WebsiteRepository
+{
+private:
+
+     std::vector<Website> websites;
+     int idCounter;
+
+public:
+
+   const std::vector<Website>& getAll() const;
+   const std::pair<bool, Website> getById(int id);
+   const std::pair<bool, Website> getByDomain(const std::string& domain) const;
+
+   bool add(const Website& website);
+
+   void update(int id, const Website& website); 
+
+};
+
+
+
+
+
+#endif
